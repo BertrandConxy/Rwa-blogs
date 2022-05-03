fe951736e5f6e37a989f15d470271784326e1a58
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-03 16:29:29 +0200

    add the posts MVC and set up linters

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class PostsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get users_index_url
+    get posts_index_url
     assert_response :success
   end
 
   test 'should get show' do
-    get users_show_url
+    get posts_show_url
     assert_response :success
   end
 end
