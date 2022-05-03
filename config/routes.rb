028f9be5f3ec4ca5eb1b291e5e2213585afba317
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-03 15:27:06 +0200

    Add the route controller and view for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  get 'users/:id', to: 'users#show'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
