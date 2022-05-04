6654649828402d5aecb2e91bef035ae816063c8a
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-04 13:00:44 +0200

    correct linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,40 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET users/:user_id/posts" do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET users/:user_id/posts' do
     before :each do
-      get user_posts_path(:user_id => 20)
+      get user_posts_path(user_id: 20)
     end
-    it "responds with status 200" do
+    it 'responds with status 200' do
       expect(response).to have_http_status(200)
     end
 
-    it "renders the posts/index view" do
-      expect(response).to render_template("posts/index")
+    it 'renders the posts/index view' do
+      expect(response).to render_template('posts/index')
     end
 
-    context "with render_views enabled" do
-      it "renders view with posts text in the index template" do
-        expect(response.body).to include("Posts")
+    context 'with render_views enabled' do
+      it 'renders view with posts text in the index template' do
+        expect(response.body).to include('Posts')
       end
     end
   end
 
-  describe "GET" do
+  describe 'GET' do
     before :each do
-    get user_post_path(:user_id => 20, :id => 2)
+      get user_post_path(user_id: 20, id: 2)
     end
-    it "responds with status 200" do
+    it 'responds with status 200' do
       expect(response).to have_http_status(200)
     end
 
-    it "renders the posts/show view" do
-      expect(response).to render_template("posts/show")
+    it 'renders the posts/show view' do
+      expect(response).to render_template('posts/show')
     end
 
-    context "with render_views enabled" do
-      it "renders view with post#2 text in the show template" do
-        expect(response.body).to include("Posts#2")
+    context 'with render_views enabled' do
+      it 'renders view with post#2 text in the show template' do
+        expect(response.body).to include('Posts#2')
       end
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,43 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /' do
     before :each do
       get users_path
     end
 
-    it "responds with status 200" do
+    it 'responds with status 200' do
       expect(response).to have_http_status(200)
     end
 
-    it "renders the users/index view" do
-      expect(response).to render_template("users/index")
+    it 'renders the users/index view' do
+      expect(response).to render_template('users/index')
     end
 
-    context "with render_views enabled" do
-      it "renders view with home page text in the index template" do
-        expect(response.body).to include("Home page")
+    context 'with render_views enabled' do
+      it 'renders view with home page text in the index template' do
+        expect(response.body).to include('Home page')
       end
     end
-    
   end
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
     before :each do
-    get user_path(:id => 20)
+      get user_path(id: 20)
     end
 
-    it "responds with status 200" do
+    it 'responds with status 200' do
       expect(response).to have_http_status(200)
     end
 
-    it "renders the users/show view" do
-      expect(response).to render_template("users/show")
+    it 'renders the users/show view' do
+      expect(response).to render_template('users/show')
     end
 
-    context "with render_views enabled" do
-      it "renders view with User profile text in the show template" do
-        expect(response.body).to include("User profile")
+    context 'with render_views enabled' do
+      it 'renders view with User profile text in the show template' do
+        expect(response.body).to include('User profile')
       end
     end
   end
