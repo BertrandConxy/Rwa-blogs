1984b621ae8ac1506137f2a4ef72ef68f09c470f
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-05 15:52:15 +0200

    add different relationships and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :user, class_name: "User", foreign_key: "author_id", :counter_cacheable: true, :counter_cache => true
+    has_many :comments, class_name: "Comment", foreign_key: true
+    has_many :likes, class_name: "Like", foreign_key: true
 end
