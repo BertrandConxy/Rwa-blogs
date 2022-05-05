0e55ad8b6389dc6a83f3f8fcdce0e9417199ac97
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-05 16:07:02 +0200

    correct linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :comments, class_name: "Comment", foreign_key: true
-    has_many :posts, class_name: "Post", foreign_key: true
-    has_many :likes, class_name: "Like", foreign_key: true
+  has_many :comments, class_name: 'Comment', foreign_key: true
+  has_many :posts, class_name: 'Post', foreign_key: true
+  has_many :likes, class_name: 'Like', foreign_key: true
 end
