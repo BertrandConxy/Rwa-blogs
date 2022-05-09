ff34cc41ef515537160ce0ead30f3fc19868acb7
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-09 10:30:26 +0200

    corrected the relations  in the models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id', counter_cache: true
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, counter_cache: true
 
   def update_comment_counter
     post.increment!(:comments_count)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id', counter_cache: true
-  has_many :comments, class_name: 'Comment', foreign_key: true
-  has_many :likes, class_name: 'Like', foreign_key: true
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id', counter_cache: true
+  has_many :comments
+  has_many :likes
 
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
