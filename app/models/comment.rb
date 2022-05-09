8a851c125e707f34bff45a4ca9ae808656723135
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-09 08:39:02 +0200

    add custom methods in all methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id', counter_cache: true
+
+  def update_comment_counter
+    post.increment!(:comments_count)
+  end
 end
