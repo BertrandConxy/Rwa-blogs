a2bc02642822c2cb793e8bfd4f230275c8769ee6
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-10 11:21:35 +0200

    created user posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @author = User.find(params[:user_id])
+    @author_posts = @author.posts
+  end
 
-  def show; end
+  def show
+
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @recent_posts = @user.recent_posts
   end
 end
