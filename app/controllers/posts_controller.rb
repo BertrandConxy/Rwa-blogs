e7ab382bbca3aa1638fde19015a94c577a6d26a9
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-10 19:55:15 +0200

    created a form to handle post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,25 @@ class PostsController < ApplicationController
     @post = @author_posts.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create(post_params)
+    @post.save
+      if @post.save
+        redirect_to current_user_path, notice: "Post has been created successfully"
+      else
+        render :new, notice: "Post could not be created"
+      end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'users#index', as: 'users'
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  # Creating a new post
+  get 'users/:user_id/posts/new', to: 'posts#new', as: 'new_post'
+  post 'users/:user_id/posts/new_post', to: 'posts#create', as: 'posts'
+  # end
   get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   get '/profile', to: 'application#current_user', as: 'current_user'
 end
