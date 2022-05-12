df68e1f98f5be07908726fd4d2ffd7411a2086db
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-12 10:57:31 +0200

    write user_spec model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates_associated :posts, :comments, :likes
   validates :name, presence: true
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(name: "Bertrand",
+                        photo: "https://randomuser.me/api/portraits/men/20.jpg",
+                        bio: "Full-stack software developer",
+                        posts_count: 0,
+                        created_at: Time.now,
+                        updated_at: Time.now)
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without valid posts_count attribute" do
+    subject.posts_count = 'one'
+    expect(subject).to_not be_valid
+  end
+
+describe "Associations" do
+  it { should have_many(:posts) }
+  it { should have_many(:comments) }
+  it { should have_many(:likes) }
+end
 end
