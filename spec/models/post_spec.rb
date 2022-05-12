a4154143680d7a82a6a4a74f1f79981fcd3f7d87
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-12 11:45:22 +0200

    write unit tests for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:author) {
+    User.new(:name => "Bertrand")
+  }
+  subject {
+    described_class.new(title: "the 10 JS best practices",
+                        text: "The following are the ten JavaScript best programming practices every devloper should know.",
+                        comments_count: 1,
+                        likes_count: 2,
+                        author: author,
+                        id: 1,
+                        created_at: Time.now,
+                        updated_at: Time.now)
+
+  }
+
+  describe "validations" do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    it "is not valid without a title" do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid with title length greater than 400" do
+      expect(subject.title.length).to be_between(1,400)
+    end
+
+    it "is not valid without valid comments_count and likes_count attribute" do
+      subject.comments_count = 'one'
+      subject.likes_count = 'one'
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without valid comments_count and likes_count integer attribute" do
+      expect(subject.comments_count).to be >= 0
+      expect(subject.likes_count).to be >= 0
+    end
+
+    
+  end
 end
