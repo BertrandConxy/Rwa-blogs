b1eda5a46251953ffb12236bbad395bea9d213f0
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-12 08:59:14 +0200

    added model-level validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, counter_cache: true
 
+  validates :author_id, :post_id, presence: true
+
   def update_likes_counter
     post.increment!(:likes_count)
   end
