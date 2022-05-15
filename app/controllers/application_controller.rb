82dabd61c39c9fedcc5aa6e0554420161a7d1b24
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-15 14:04:58 +0200

    run migrations for adding devise to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @user = User.first
-  end
-  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'users#index', as: 'users'
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
